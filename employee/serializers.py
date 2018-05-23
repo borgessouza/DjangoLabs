57da09b7e0aceef3e6992b5c1f3cfc19f0bb0702
borgessouza <borgessouza@gmail.com>
2018-05-23 00:33:44 -0300

    Initial work

---
+++ employee/serializers.py
@@ -0,0 +1,8 @@
+from .models import Employee
+from rest_framework import serializers
+
+
+class employee_serializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Employee
+        fields = ('name', 'department')
